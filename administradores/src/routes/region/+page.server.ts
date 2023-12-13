ced31435b5c53bbc71e0342af76598be87544ee5
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-12-13 18:24:07 -0300

    Insertar regiones desde desde frontend

--- administradores/src/routes/region/+page.server.ts
+++ administradores/src/routes/region/+page.server.ts
@@ -1,12 +1,56 @@
 import { API_URL } from "$env/static/private";
+import { fail } from "@sveltejs/kit";
+
+type Region = {
+    id: number;
+    nombre: string;
+    numero: number;
+};
 
 export async function load() {
-    const response = await fetch(`${API_URL}/region`)
+    const response = await fetch(`${API_URL}/region`);
     if (!response.ok) {
-        return { regiones: [] }
+        return { regiones: [] };
     }
-    const regiones = await response.json()
-    return {
-        regiones: regiones
+    const regiones: Region[] = await response.json();
+    return { regiones: regiones };
+}
+
+export const actions = {
+    insert: async ({ request }) => {
+        const data = await request.formData();
+        const nombre = data.get("nombre");
+        let numero = data.get("numero")?.valueOf();
+        const form = {
+            nombre: nombre,
+            numero: numero,
+            error: ""
+        };
+
+        if (nombre === null || nombre === "") {
+            form.error = "Ingrese un nombre";
+            return fail(400, form);
+        }
+        if (typeof numero !== "string") {
+            form.error = "Ingrese un número";
+            return fail(400, form);
+        }
+        if (numero === "") {
+            form.error = "Ingrese un número";
+            return fail(400, form);
+        }
+        numero = parseInt(numero);
+
+        const req = await fetch(`${API_URL}/region`, {
+            method: "POST",
+            body: JSON.stringify({ nombre, numero }),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        if (!req.ok) {
+            const res = await req.json();
+            form.error = res["error"];
+            return fail(400, form);
+        }
     }
 }
--- administradores/src/routes/region/+page.svelte
+++ administradores/src/routes/region/+page.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
     import type { PageData } from "./$types";
     export let data: PageData;
+    export let form;
 </script>
 
 <h1>Región</h1>
+
+<p>{form?.error || ""}</p>
+<form method="post" action="?/insert">
+    <input
+        type="text"
+        placeholder="Nombre"
+        name="nombre"
+        value={form?.nombre || ""}
+    />
+    <input
+        type="number"
+        placeholder="Numero"
+        name="numero"
+        min="0"
+        value={form?.numero || ""}
+    />
+    <input type="submit" value="Insertar" />
+</form>
+
 <table border="1">
     <thead>
         <th>Nombre</th>
