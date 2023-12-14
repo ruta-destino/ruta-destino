a1f02f168e9fca990e1cb57a4d24e1f5fd5bb85d
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-12-14 17:20:45 -0300

    Actualizar ciudades

---
+++ administradores/src/routes/ciudad/[id]/+page.server.ts
@@ -0,0 +1,69 @@
+import { API_URL } from "$env/static/private";
+import { error, fail, redirect } from "@sveltejs/kit";
+import type { Ciudad, Provincia } from "$lib/server/models.js";
+
+export async function load({ params }) {
+    const id = params.id;
+    let req = await fetch(`${API_URL}/ciudad/${id}`);
+    if (!req.ok) {
+        throw error(404, "Not Found");
+    }
+    const ciudad: Ciudad = await req.json();
+    req = await fetch(`${API_URL}/provincia`);
+    if (!req.ok) {
+        throw error(404, "Not Found");
+    }
+    const provincias: Provincia[] = await req.json()
+    const data = {
+        provincias,
+        ciudad
+    }
+    return data;
+}
+
+const form: { [key: string]: FormDataEntryValue | null } = {
+    nombre: null,
+    id_provincia: null,
+    error: null
+};
+
+export const actions = {
+    update: async ({ request }) => {
+        const data = await request.formData();
+        const f_id = data.get("id");
+        const f_nombre = data.get("nombre");
+        const f_id_provincia = data.get("id_provincia");
+        form.nombre = f_nombre;
+        form.id_provincia = f_id_provincia;
+
+        if (typeof f_nombre !== "string" || f_nombre === "") {
+            form.error = "Ingrese un nombre";
+            return fail(400, form);
+        }
+        const nombre = f_nombre;
+
+        if (typeof f_id_provincia !== "string" || f_id_provincia === "") {
+            form.error = "Seleccione una región";
+            return fail(400, form);
+        }
+        const id_provincia = parseInt(f_id_provincia);
+        if (isNaN(id_provincia)) {
+            form.error = "Seleccione una provincia válida";
+            return fail(400, form);
+        }
+
+        const req = await fetch(`${API_URL}/ciudad/${f_id}`, {
+            method: "POST",
+            body: JSON.stringify({ nombre, id_provincia }),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        if (!req.ok) {
+            const res = await req.json();
+            form.error = res["error"];
+            return fail(400, form);
+        }
+
+        throw redirect(302, "/ciudad")
+    }
+}
---
+++ administradores/src/routes/ciudad/[id]/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type { PageData } from "./$types";
+    export let data: PageData;
+    export let form;
+</script>
+
+<h1>Ciudad</h1>
+<a href="/ciudad">Ver ciudades</a>
+<p>{form?.error || ""}</p>
+<form method="post" action="?/update">
+    <select name="id_provincia">
+        <option value="">---</option>
+        {#each data.provincias as provincia (provincia.id)}
+            {#if data.ciudad.id_provincia === provincia.id}
+                <option value={provincia.id} selected>{provincia.nombre}</option>
+            {:else}
+                <option value={provincia.id}>{provincia.nombre}</option>
+            {/if}
+        {/each}
+    </select>
+    <input
+        type="text"
+        placeholder="Nombre"
+        name="nombre"
+        value={form?.nombre ?? data.ciudad.nombre}
+    />
+    <input type="hidden" name="id" value={data.ciudad.id} />
+    <input type="submit" value="Actualizar" />
+</form>
