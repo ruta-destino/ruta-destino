887fc9cf7c26bf8c3f1e80d9be7290648c75058e
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-12-15 14:15:21 -0300

    Base del frontend para usuarios

---
+++ usuarios/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+    <a href="/">Inicio</a>
+    <a href="/1">Sitio 1</a>
+    <a href="/2">Sitio 2</a>
+</nav>
+
+<slot />
---
+++ usuarios/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Zona de usuarios</h1>
+<p>
+    Esta interfaz permite explorar los diferentes recorridos que ofrecen las
+    empresas de buses dentro de la aplicación Ruta Destino.
+</p>
