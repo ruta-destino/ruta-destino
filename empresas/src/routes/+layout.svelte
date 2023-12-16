4ec2a586ff6a031ff6a24dfdd2dbb238206e525c
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-12-16 19:54:12 -0300

    Base del frontend para empresas

---
+++ empresas/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<nav>
+    <a href="/">Inicio</a>
+</nav>
+
+<slot />
---
+++ empresas/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Zona de empresas</h1>
+<p>
+    Por medio de esta interfaz podrá controlar todos sus datos, para así poder
+    compartirlos con el resto de los usuarios que deseen consultar la
+    información aquí presente.
+</p>
