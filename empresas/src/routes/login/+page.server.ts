4cea6faa8fe31f24bbeb54cedda1cfa377475d10
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-12-17 12:02:29 -0300

    Aumentar duración de las cookies de inicio a un día

--- empresas/src/routes/login/+page.server.ts
+++ empresas/src/routes/login/+page.server.ts
@@ -30,8 +30,14 @@ export const actions = {
         };
 
         const empresa: Empresa = await req.json();
-        cookies.set("empresa_id", String(empresa.id), { path: "/" });
-        cookies.set("empresa_nombre", String(empresa.nombre), { path: "/" });
+        cookies.set("empresa_id", String(empresa.id), {
+            path: "/",
+            maxAge: 86400
+        });
+        cookies.set("empresa_nombre", String(empresa.nombre), {
+            path: "/",
+            maxAge: 86400
+        });
 
         throw redirect(302, "/");
     }
